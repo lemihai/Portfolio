a8ce42f36c40a5e40a02246d8a62dee11100f6b0
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2024-03-01 15:13:33 +0100

    dev-1.1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,17 @@ export default function App() {
 }
 
 function Nav() {
-  return <nav>nav</nav>;
+  return (
+    <nav>
+      <div className="logo">LOGO</div>
+      <div>
+        <a href="#">home</a>
+        <a href="#">work</a>
+        <a href="#">about</a>
+        <a href="#">contact</a>
+      </div>
+    </nav>
+  );
 }
 
 function Footer() {
@@ -17,6 +27,18 @@ function Footer() {
     // add the instagram icon and the link to the instagram page
     // the link should open in a new tab
     <footer>
+      <a href="https://www.instagram.com/le_mihai/?hl=en">
+        <img
+          height="20px"
+          src="../images/icons/instagram.png"
+          alt="instagram"
+        />
+        instagram
+      </a>
+      <a href="https://www.instagram.com/le_mihai/?hl=en">
+        <img height="20px" src="../images/icons/instagram.png" alt="Linkedin" />
+        instagram
+      </a>
       <a href="https://www.instagram.com/le_mihai/?hl=en">
         <img
           height="20px"
