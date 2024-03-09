bdabb8ef16462a74db11428d719e595c0786d2b0
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2024-03-09 18:29:17 +0100

    dev-1.5 (background)
    worked the background by adding bubbles, gaussian blurring and the scss for the them. Installed Scss

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from "react";
 // IMPORT ROUTER
 import {
   BrowserRouter as Router,
@@ -13,15 +14,29 @@ import Nav from "./Global/components/nav";
 import Footer from "./Global/components/footer";
 import Hero from "./Pages/main/components/hero";
 import Carousel from "./Pages/main/components/carousel";
+import CarouselB from "./Pages/main/components/carouselB";
 
 // PAGES import
 import Work from "./Pages/work/work";
 import Contact from "./Pages/contact/contact";
 import About from "./Pages/about/about";
 
+import "./style.scss";
+
 export default function App() {
+  // CIRCLE FOLLOWING MOUSE
+  // NOT WORKING
   return (
-    <div className="body background-gradient">
+    <div className="body gradient-bg">
+      <GooFilter />
+      <div className="gradients-container">
+        <div className="g1"></div>
+        <div className="g2"></div>
+        <div className="g3"></div>
+        <div className="g4"></div>
+        <div className="g5"></div>
+        <div className="interactive"></div>
+      </div>
       <BrowserRouter>
         <Nav />
 
@@ -34,7 +49,27 @@ export default function App() {
       </BrowserRouter>
       <Hero />
       <Carousel />
+      <CarouselB />
       <Footer />
     </div>
+  );
+}
+
+function GooFilter() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+          <feColorMatrix
+            in="blur"
+            mode="matrix"
+            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+            result="goo"
+          />
+          <feBlend in="SourceGraphic" in2="goo" />
+        </filter>
+      </defs>
+    </svg>
   );
 }
---
+++ src/Pages/main/components/carouselB.js
@@ -0,0 +1,89 @@
+import { Tilt } from "react-tilt";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "../swiper.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
+
+export default function CarouselB() {
+  return (
+    <>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Tilt options={defaultOptions}>
+            <div className="project-wrap">
+              <p className="project ">PS2</p>
+              <p className="project-description">university project</p>
+            </div>{" "}
+          </Tilt>{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Tilt options={defaultOptions}>
+            <div className="project-wrap">
+              <p className="project ">PS2</p>
+              <p className="project-description">university project</p>
+            </div>{" "}
+          </Tilt>{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Tilt options={defaultOptions}>
+            <div className="project-wrap">
+              <p className="project ">PS2</p>
+              <p className="project-description">university project</p>
+            </div>{" "}
+          </Tilt>{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Tilt options={defaultOptions}>
+            <div className="project-wrap">
+              <p className="project ">PS2</p>
+              <p className="project-description">university project</p>
+            </div>{" "}
+          </Tilt>{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Tilt options={defaultOptions}>
+            <div className="project-wrap">
+              <p className="project ">PS2</p>
+              <p className="project-description">university project</p>
+            </div>{" "}
+          </Tilt>{" "}
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
