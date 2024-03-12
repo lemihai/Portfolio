caa744302d8718251f08f33b4da8e4faf27d4f27
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2024-03-12 11:10:55 +0100

    dev 1.7.2 (projects json and router)
    in this update I created a json where I will store the data about the projects. I connected it through the router and I used props to give each project component a path. Moreover, I restyled the work page by using grid and I created 3 project pages that I will fill in soon

--- src/Pages/work/work.js
+++ src/Pages/work/work.js
@@ -1,5 +1,7 @@
 import example from "../../Images/grad.jpg";
 import "./work.css";
+import projectList from "../../Content/data.json";
+import { Link, Routes, Route } from "react-router-dom";
 
 export default function Work() {
   return (
@@ -16,29 +18,45 @@ export default function Work() {
           <ButtonFilterProject text="Programming" />
           <ButtonFilterProject text="Research" />
         </div>
-        <div className="work-projects-wrap">
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-        </div>
+        {/* <BrowserRouter> */}
+        <ProjectList />
+        <Routes>
+          <Route path="ps2" element={<Project key={1} />}></Route>
+          <Route path="grad" element={<Project key={2} />}></Route>
+          <Route path="grad-design" element={<Project key={3} />}></Route>
+        </Routes>
+        {/* </BrowserRouter> */}
       </div>
     </main>
   );
 }
 
-function Project() {
+function ProjectList() {
+  const projectsLS = projectList.map((project) => (
+    <Project
+      key={project.id}
+      name={project.project_name}
+      description={project.description}
+      pagelink={project.pagelink}
+    />
+  ));
+
+  return <div className="work-projects-wrap">{projectsLS}</div>;
+}
+
+function Project({ name, description, pagelink }) {
   return (
-    <div className="work-project">
-      <img
-        className="work-project-image"
-        src={example}
-        alt="project title image5"
-      ></img>
-      <h2 className="work-project-title">Title</h2>
-      <p className="work-project-text">Example</p>
-    </div>
+    <Link to={pagelink}>
+      <div className="work-project">
+        <img
+          className="work-project-image"
+          src={example}
+          alt="project title image5"
+        ></img>
+        <h2 className="work-project-title">{name}</h2>
+        <p className="work-project-text">{description}</p>
+      </div>
+    </Link>
   );
 }
 
