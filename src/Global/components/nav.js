7869a7ac7ca23bda2cc97031965e7f9d040c49ef
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2024-03-27 00:46:33 +0100

    dev-1.11 mobile nav
    added the mobile navigation css and used state for the open and close button

--- src/Global/components/nav.js
+++ src/Global/components/nav.js
@@ -1,14 +1,28 @@
 import { Tilt } from "react-tilt";
 import { Link } from "react-router-dom";
 import logo from "../../Images/icons/Logo.png";
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 export default function Nav() {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const handleNavOpen = () => {
+    setNavOpen(true);
+  };
+
+  const handleNavClose = () => {
+    setNavOpen(false);
+  };
+
   return (
-    <nav>
+    <nav className={`${navOpen === true ? "nav-open" : ""}`}>
       <Link to="/pages/main">
         <img className="logo" src={logo} alt="logo" />
       </Link>
-      <div className="nav-flex">
+      {/* conditionally render with ternary operator the nav open */}
+      <div className="nav-flex ">
         <div className="nav-link">
           <Tilt>
             <Link className="nav-btn" to="/pages/main">
@@ -38,6 +52,15 @@ export default function Nav() {
           </Tilt>
         </div>
       </div>
+
+      <button className="mobile-nav-btn">
+        <IoMenu className="icon-menu" onClick={handleNavOpen} name="menu" />
+        <IoMdClose
+          className="icon-menu"
+          onClick={handleNavClose}
+          name="close"
+        />
+      </button>
     </nav>
   );
 }
